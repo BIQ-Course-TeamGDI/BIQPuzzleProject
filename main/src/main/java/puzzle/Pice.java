8d9656cf80a31d275029d2a6151441106c660e24
Doron Niv <dn141b@intl.att.com>
2018-03-27 16:59:56 +0300

    Add guy Pice enum class

---
+++ main/src/main/java/puzzle/Pice.java
@@ -0,0 +1,52 @@
+package puzzle;
+
+import java.util.HashMap;
+
+enum EnumSides
+{
+    TOP, BOTTOM, RIGHT, LEFT;
+};
+public class Pice {
+
+    public static int numOfElements = 0;
+    private int id;
+    private HashMap<EnumSides, Integer> pice = new HashMap<EnumSides, Integer>();
+
+    public Pice(int id, HashMap<EnumSides, Integer> shape)
+    {
+        super();
+        this.id = id;
+        this.pice = shape;
+        numOfElements++;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getBottom()
+    {
+        return pice.get(EnumSides.BOTTOM);
+    }
+
+    public int getTop()
+    {
+        return pice.get(EnumSides.TOP);
+    }
+
+    public int getLeft()
+    {
+        return pice.get(EnumSides.LEFT);
+    }
+
+    public int getRight()
+    {
+        return pice.get(EnumSides.RIGHT);
+    }
+}
