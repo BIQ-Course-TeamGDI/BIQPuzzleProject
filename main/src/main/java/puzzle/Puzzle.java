e02a2f21b84140f205b017f4fa82672ce5130c69
Doron Niv <dn141b@intl.att.com>
2018-03-27 17:23:26 +0300

    Create Puzzle class

---
+++ main/src/main/java/puzzle/Puzzle.java
@@ -0,0 +1,15 @@
+package puzzle;
+
+import java.util.ArrayList;
+
+public class Puzzle {
+
+
+    private ArrayList<Piece> puzzlePieces;
+
+
+    public Puzzle(ArrayList<Piece> puzzlePieces ){
+        this.puzzlePieces=puzzlePieces;
+    }
+
+}
