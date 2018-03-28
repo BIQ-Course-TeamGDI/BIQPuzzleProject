42926d75fc0ea98a7cbd8f8918e90e231178d99c
gb824v <gb824v@intl.att.com>
2018-03-28 15:53:20 +0300

    [US00001] Refactoring according to lattice reportportal

--- main/src/main/java/puzzle/FileManagment.java
+++ main/src/main/java/puzzle/FileManagment.java
@@ -3,17 +3,20 @@ package puzzle;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
-public class FileManagment
+import org.apache.log4j.Logger;
+
+public class FileManagment implements ParamsInterface
 {
 	private File fileInput;
 	private int numElements = 0;
-	private int[][] pieces;
-	private int lineNumber = 0;
+	private ArrayList<Piece> pieces = new ArrayList<Piece>();
+	final static Logger logger = Logger.getLogger(FileManagment.class);
 
 	public FileManagment(String inputFilePath)
 	{
@@ -29,71 +32,138 @@ public class FileManagment
 				BufferedReader br = new BufferedReader(new InputStreamReader(fis));
 				String sCurrentLine;
 				sCurrentLine = br.readLine();
-				if (isNumElementsValid(sCurrentLine))
+				while ((sCurrentLine = br.readLine()) != null)
 				{
-					pieces = new int[numElements][5];
-					
-					while ((sCurrentLine = br.readLine()) != null && !sCurrentLine.startsWith("#"))
+					if (!sCurrentLine.startsWith("#") && isNumElementsValid(sCurrentLine))
 					{
-						if (isLineValid(sCurrentLine))
+						int[] piece = getLineAsPiece(sCurrentLine);
+
+						if (piece != null)
 						{
-							addPiece(sCurrentLine);
+							addPiece(piece);
 						}
 
 					}
 				}
-				else
-				{
-					return;
-				}
-
 			}
 		}
-	}
-
-	private boolean isLineValid(String sCurrentLine)
-	{
-		return true;
 	}
 
 	private boolean isNumElementsValid(String sCurrentLine)
 	{
-		if (sCurrentLine.startsWith("NumElements="))
+		if (numElements == 0)
 		{
-			String[] numElementsArr = sCurrentLine.split("=");
-			String value = numElementsArr[1].trim();
+			if (sCurrentLine.startsWith(NUM_ELEMENTS_STR))
+			{
+				String[] numElementsArr = sCurrentLine.split("=");
+				String value = numElementsArr[1].trim();
 
-			if (value.isEmpty())
-			{
-				return false;
-			}
-			else
-			{
-				try
-				{
-					this.numElements = Integer.parseInt(value);
-				}
-				catch (Exception e)
+				if (value.isEmpty())
 				{
 					return false;
 				}
-
+				else
+				{
+					try
+					{
+						this.numElements = Integer.parseInt(value);
+						return true;
+					}
+					catch (Exception e)
+					{
+						return false;
+					}
+				}
 			}
-
+			else
+			{
+				return false;
+			}
 		}
-		return true;
+		else
+		{
+			return true;
+		}
 	}
 
-	private void addPiece(int[] piece)
+	private int[] getLineAsPiece(String sCurrentLine)
 	{
-		int[] piece;
-		this.pieces[lineNumber] = piece;
+		int[] goodPiece = new int[SIDES];
+
+		if (sCurrentLine.startsWith(NUM_ELEMENTS_STR))
+		{
+			return null;
+		}
+		else
+		{
+			String[] lineArr = sCurrentLine.split(SEPARATOR);
+			if (lineArr.length == 5)
+			{
+				int i = 0;
+				for (String str : lineArr)
+				{
+					try
+					{
+						goodPiece[i++] = Integer.parseInt(str);
+					}
+					catch (Exception e)
+					{
+						return null;
+					}
+
+				}
+			}
+		}
+		return goodPiece;
+	}
+
+	private void addPiece(int[] pieceArr)
+	{
+		int id = pieceArr[0];
+		HashMap<EnumSides, Integer> pieceMap = new HashMap<EnumSides, Integer>();
+		pieceMap.put(EnumSides.LEFT, pieceArr[1]);
+		pieceMap.put(EnumSides.TOP, pieceArr[2]);
+		pieceMap.put(EnumSides.RIGHT, pieceArr[3]);
+		pieceMap.put(EnumSides.BOTTOM, pieceArr[4]);
+		pieces.add(new Piece(id, pieceMap));
+	}
+
+	public ArrayList<Piece> getPieces()
+	{
+		if (isIdsAndSizeValids())
+		{
+			return pieces;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public boolean isIdsAndSizeValids()
+	{
+		if (pieces.size() == numElements)
+		{
+			Collections.sort(pieces);
+			for (int i = 0; i <= numElements; i++)
+			{
+				if (pieces.get(i).getId() != i)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	public static void main(String[] args) throws IOException
 	{
 		FileManagment fileManagment = new FileManagment("input.txt");
 		fileManagment.loadAsText();
+		fileManagment.getPieces();
 	}
-
 }
---
+++ main/src/main/java/puzzle/ParamsInterface.java
@@ -0,0 +1,8 @@
+package puzzle;
+
+public interface ParamsInterface
+{
+	static final int SIDES = 5;
+	static final String NUM_ELEMENTS_STR = "NumElements=";
+	static final String SEPARATOR = " ";
+}
