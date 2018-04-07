a0fa60a7a47250ca34c7be6381912e4094a008a6
Wallerstein, Ilan (iw4360) <iw4360@intl.att.com>
2018-04-07 09:19:21 +0300

    AnalyzeInputs updates

--- main/src/main/java/puzzle/AnalyzeInputs.java
+++ main/src/main/java/puzzle/AnalyzeInputs.java
@@ -7,6 +7,15 @@ import java.util.Collections;
 
 import org.apache.log4j.Logger;
 
+import puzzleSolver.PuzzleSolver;
+
+/**
+ * @author iw4360
+ *
+ *This class will analyze the pieces that came as input to check their validation
+ * for the solution before starting to find the solution  
+ */
+
 public class AnalyzeInputs {
 
 	private ArrayList<Piece> pieces = new ArrayList<Piece>();
@@ -16,49 +25,104 @@ public class AnalyzeInputs {
 		validateEdgesSum(input);
 		validatePiecesIds(input);
 		validatePiecesFormat(input);
-		validateMinimumCorners(input);
-		validateMinimumStraightEdges(input);
+		ArrayList<Integer> rows = validateMinimumStraightEdges(input);
+		validateMinimumCorners(input, rows);
 
 		return null;
 
 	}
 
-	private void validateMinimumStraightEdges(ArrayList<Piece> input) {
+	
+	/**
+	 * @param input: ArrayList
+	 * @return rows: ArrayList
+	 * @see This method run all over the possible puzzle structure and looking for the minimum straight
+	 * edges for each structure. If found, it add this option to a list that will be forward to the solver 
+	 */
+	private ArrayList<Integer> validateMinimumStraightEdges(ArrayList<Piece> input) {
 		// TODO Auto-generated method stub
+		int leftZeroEdges = 0, topZeroEdges = 0, rightZeroEdges = 0, bottomZeroEdges = 0;
+		ArrayList<Integer> optionalRowsForSolution = new ArrayList<>();
+		ArrayList<Integer> possibleSolutionRows = PuzzleSolver.getPossibleSolutionRows(input.size());
+		for (int numOfRows : possibleSolutionRows) {
+			int numOfColumns = input.size() / numOfRows;
+			for (Piece p : input) {
+				if (p.getLeft() == 0)
+					leftZeroEdges++;
+				if (p.getTop() == 0)
+					topZeroEdges++;
+				if (p.getRight() == 0)
+					rightZeroEdges++;
+				if (p.getBottom() == 0)
+					bottomZeroEdges++;
+				if (leftZeroEdges >= numOfRows && topZeroEdges >= numOfColumns && rightZeroEdges >= numOfRows
+						&& bottomZeroEdges >= numOfColumns) {
+					optionalRowsForSolution.add(numOfRows);
+					break;
 
-	}
-
-	private void validateMinimumCorners(ArrayList<Piece> input) {
-		// TODO Auto-generated method stub
-		boolean leftTopCorner = false, topRightCorner = false, rightBottomCorner = false, bottomLeftCorner = false;
-		int index = 0;
-
-		while(!leftTopCorner && !topRightCorner && !rightBottomCorner && !bottomLeftCorner) {
-			if(index > input.size())
-				break;
-			else {
-				//if isPrime == need to check one row corners, else not in the same row 
-				if(input.size()==1) {
-					
-				}
-				else if(isPrime(input.size()) ) {
-					
-				}
-				else {
-					
 				}
 			}
-			index++;
-			
+
 		}
-
+		return optionalRowsForSolution;
 	}
 
-	private boolean isPrime(int size) {
+	/**
+	 * @param input:ArrayList
+	 * @param rows: ArrayList
+	 * @return optionalRows: ArrayList
+	 * @see: This method run all over the possible puzzle structure [after it was checked that it has minimum straight edges] and looking for the minimum corners
+	 * for each structure. If found, it add this option to a list that will be forward to the solver 
+	 */
+	private ArrayList<Integer> validateMinimumCorners(ArrayList<Piece> input, ArrayList<Integer> rows) {
 		// TODO Auto-generated method stub
-		return false;
+		boolean leftTopCorner = false, topRightCorner = false, rightBottomCorner = false, bottomLeftCorner = false;
+		ArrayList<Integer> optionalRowsForSolution = new ArrayList<>();
+
+		for (int numOfRows : rows) {
+			int numOfColumns = input.size() / numOfRows;
+			for (Piece p : input) {
+				if (p.getLeft() == 0 && p.getTop() == 0)
+					leftTopCorner = true;
+				if (p.getTop() == 0 && p.getRight() == 0)
+					topRightCorner = true;
+				if (p.getRight() == 0 && p.getBottom() == 0)
+					rightBottomCorner = true;
+				if (p.getBottom() == 0 && p.getLeft() == 0)
+					bottomLeftCorner = true;
+				if (leftTopCorner && topRightCorner && rightBottomCorner && bottomLeftCorner) {
+					optionalRowsForSolution.add(numOfRows);
+					break;
+
+				}
+			}
+
+		}
+		return optionalRowsForSolution;
+
+		/*
+		 * int index = 0;
+		 * 
+		 * while (!leftTopCorner && !topRightCorner && !rightBottomCorner &&
+		 * !bottomLeftCorner) { if (index > input.size()) break; else { // if isPrime ==
+		 * need to check one row corners, else not in the same row if (input.size() ==
+		 * 1) {
+		 * 
+		 * } else if (isPrime(input.size())) {
+		 * 
+		 * } else {
+		 * 
+		 * } } index++;
+		 * 
+		 * }
+		 */
 	}
 
+	
+	/**
+	 * @param input: ArrayList
+	 * @see: This method check that input pieces edges arein the range of -1 to 1
+	 */
 	private void validatePiecesFormat(ArrayList<Piece> input) {
 		// TODO Auto-generated method stub
 		for (Piece p : input) {
@@ -69,7 +133,11 @@ public class AnalyzeInputs {
 		}
 	}
 
-	private void validateEdgesSum(ArrayList<Piece> input) {
+	/**
+	 * @param input: ArrayList
+	 * @see: This method check that input pieces edges sum is 0 --> the the shape is closed
+	 */
+	public static void validateEdgesSum(ArrayList<Piece> input) {
 		// TODO Auto-generated method stub
 		for (Piece p : input) {
 			if (p.getRight() + p.getTop() + p.getBottom() + p.getLeft() != 0) {
@@ -80,7 +148,11 @@ public class AnalyzeInputs {
 
 	}
 
-	private void validatePiecesIds(ArrayList<Piece> input) {
+	/**
+	 * @param input: ArrayList
+	 * @see: This method check that input pieces ids is correct
+	 */
+	public static void validatePiecesIds(ArrayList<Piece> input) {
 		// TODO Auto-generated method stub
 		Arrays.sort(input.toArray());
 		Collections.sort(input);
@@ -91,7 +163,7 @@ public class AnalyzeInputs {
 		}
 	}
 
-	private void checkPiecesSequences(ArrayList<Piece> input) {
+	private static void checkPiecesSequences(ArrayList<Piece> input) {
 		// TODO Auto-generated method stub
 
 	}
---
+++ main/src/test/java/AnalyzeinputTests/AnalyzeinputTests.java
@@ -0,0 +1,151 @@
+package AnalyzeinputTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import puzzle.AnalyzeInputs;
+import puzzle.Piece;
+
+class AnalyzeinputTests {
+
+	@BeforeEach
+	public void setup() {
+		Piece p1 = Mockito.mock(Piece.class);
+		Mockito.when(p1.getLeft()).thenReturn(0);
+		Mockito.when(p1.getTop()).thenReturn(0);
+		Mockito.when(p1.getRight()).thenReturn(0);
+		Mockito.when(p1.getBottom()).thenReturn(0);
+
+		ArrayList<Piece> pcs = new ArrayList<>();
+		pcs.add(p1);
+	}
+
+	@Test
+	void ValidateSumOfEdgesGoodTest() {
+		// TODO: sum edges for a single piece and assert is zero
+
+		Piece p1 = Mockito.mock(Piece.class);
+		Mockito.when(p1.getLeft()).thenReturn(0);
+		Mockito.when(p1.getTop()).thenReturn(0);
+		Mockito.when(p1.getRight()).thenReturn(0);
+		Mockito.when(p1.getBottom()).thenReturn(0);
+
+		ArrayList<Piece> pcs = new ArrayList<>();
+		pcs.add(p1);
+		AnalyzeInputs.validateEdgesSum(pcs);
+		// need to add assert
+	}
+
+	@Test
+	void ValidateSumOfEdgesBadTest() {
+		// TODO: 5,-5,0,0
+
+		Piece p1 = Mockito.mock(Piece.class);
+		Mockito.when(p1.getLeft()).thenReturn(5);
+		Mockito.when(p1.getTop()).thenReturn(-5);
+		Mockito.when(p1.getRight()).thenReturn(0);
+		Mockito.when(p1.getBottom()).thenReturn(0);
+		ArrayList<Piece> pcs = new ArrayList<>();
+		pcs.add(p1);
+		AnalyzeInputs.validateEdgesSum(pcs);
+		// need to add assert
+	}
+
+	@Test
+	void ValidatePieceFormatGoodTest() {
+		// TODO: 0,0,1,0
+
+		Piece p1 = Mockito.mock(Piece.class);
+		Mockito.when(p1.getLeft()).thenReturn(0);
+		Mockito.when(p1.getTop()).thenReturn(0);
+		Mockito.when(p1.getRight()).thenReturn(1);
+		Mockito.when(p1.getBottom()).thenReturn(0);
+		ArrayList<Piece> pcs = new ArrayList<>();
+		pcs.add(p1);
+		AnalyzeInputs.validateEdgesSum(pcs);
+		// need to add assert
+
+	}
+
+	@Test
+	void ValidatePieceFormatBadTest() {
+		// TODO: 0,0,2,0
+
+		Piece p1 = Mockito.mock(Piece.class);
+		Mockito.when(p1.getLeft()).thenReturn(0);
+		Mockito.when(p1.getTop()).thenReturn(2);
+		Mockito.when(p1.getRight()).thenReturn(0);
+		Mockito.when(p1.getBottom()).thenReturn(0);
+		ArrayList<Piece> pcs = new ArrayList<>();
+		pcs.add(p1);
+		AnalyzeInputs.validateEdgesSum(pcs);
+		// need to add assert
+
+	}
+
+	@Test
+	void ValidateWrongPiecesIdsBadTest() {
+		// TODO: puzzle with 7 pieces have id:9
+		Piece p1 = Mockito.mock(Piece.class);
+		Mockito.when(p1.getId()).thenReturn(5);
+		Piece p2 = Mockito.mock(Piece.class);
+		Mockito.when(p2.getId()).thenReturn(2);
+	
+		ArrayList<Piece> pcs = new ArrayList<>();
+		pcs.add(p1);
+		pcs.add(p2);
+		AnalyzeInputs.validatePiecesIds(pcs);
+		// need to add assert
+
+	}
+
+	@Test
+	void ValidateMissingPiecesIdsBadTest() {
+		// TODO: puzzle with ids :1,3,9
+		Piece p1 = Mockito.mock(Piece.class);
+		Mockito.when(p1.getId()).thenReturn(1);
+		Piece p2 = Mockito.mock(Piece.class);
+		Mockito.when(p2.getId()).thenReturn(2);
+		Piece p3 = Mockito.mock(Piece.class);
+		Mockito.when(p3.getId()).thenReturn(4);
+	
+		ArrayList<Piece> pcs = new ArrayList<>();
+		pcs.add(p1);
+		pcs.add(p2);
+		AnalyzeInputs.validatePiecesIds(pcs);
+	
+	}
+
+	@Test
+	void ValidateWrongNumberOfStraightEdgesBadTest() {
+		// TODO: wrong number of straight edges
+
+	}
+
+	@Test
+	void ValidateWrongNumberOfStraightEdgesGoodTest() {
+		// TODO: minimum+ number of straight edges
+
+	}
+
+	@Test
+	void ValidatePiecesCornersGoodTest() {
+		// TODO: minimum+ corner
+
+	}
+
+	@Test
+	void ValidatePiecesCornersBaddTest() {
+		// TODO: > corners
+
+	}
+}
