5875e9264ba238bea46fa3ff6ff3c693fc2f6eb1
Doron Niv <dn141b@intl.att.com>
2018-04-12 13:20:36 +0300

    Fixes

--- main/src/test/java/AnalyzeinputTests/AnalyzeinputTests.java
+++ main/src/test/java/AnalyzeinputTests/AnalyzeinputTests.java
@@ -1,9 +1,9 @@
 package AnalyzeinputTests;
 
-
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 
@@ -12,198 +12,202 @@ import infra.FileManager;
 import org.junit.Test;
 import puzzle.AnalyzeInputs;
 import puzzle.Piece;
-import puzzle.Puzzle;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+public class AnalyzeinputTests {
 
-class AnalyzeinputTests {
-	ArrayList<String> errors = new ArrayList<>();
 
-	@Test
-	void ValidateSumOfEdgesGoodTest() {
-		// Sum of edges for a single piece and assert is zero
+    @Test
+        //@ParameterizedTest(name = "ValidateSumOfEdgesGoodTest{index}: Values:{0}")
+        //@CsvSource({ "13", "31", "133", "541", "1" })
+    public void ValidateSumOfEdgesGoodTest() {
+        // Sum of edges for a single piece and assert is zero
 
-		HashMap<EnumSides, Integer> edges = new HashMap<>();
-		edges.put(EnumSides.LEFT, 0);
-		edges.put(EnumSides.TOP, 0);
-		edges.put(EnumSides.RIGHT, 0);
-		edges.put(EnumSides.BOTTOM, 0);
-		Piece pc1 = new Piece(10, edges);
+        HashMap<EnumSides, Integer> edges = new HashMap<>();
+        edges.put(EnumSides.LEFT, 0);
+        edges.put(EnumSides.TOP, 0);
+        edges.put(EnumSides.RIGHT, 0);
+        edges.put(EnumSides.BOTTOM, 0);
+        Piece pc1 = new Piece(10, edges);
+        ArrayList<Piece> pcs = new ArrayList<>();
+        pcs.add(pc1);
+        FileManager fileManager = new FileManager("");
+        AnalyzeInputs analyze = new AnalyzeInputs(pcs);
 
-		ArrayList<Piece> pcs = new ArrayList<>();
-		pcs.add(pc1);
-		AnalyzeInputs analyzeInputs = new AnalyzeInputs(pcs);
-		analyzeInputs.analyzePicesList();
-		errors = FileManager.
-		assertFalse(errors.contains("Number of straight edges is invalid:10"));
-	}
+        analyze.validateEdgesSum();
+        assertFalse(fileManager.getAllErrors().contains("Number of straight edges is invalid:10"));
+    }
 
-	@Test
-	void ValidateSumOfEdgesBadTest() {
-		// Sum of edges for single piece is not zero
-		errors.clear();
+    @Test
+    public void ValidateSumOfEdgesBadTest() {
+        // Sum of edges for single piece is not zero
+        HashMap<EnumSides, Integer> edges = new HashMap<>();
+        edges.put(EnumSides.LEFT, 1);
+        edges.put(EnumSides.TOP, -1);
+        edges.put(EnumSides.RIGHT, 1);
+        edges.put(EnumSides.BOTTOM, 1);
+        Piece pc1 = new Piece(10, edges);
 
-		HashMap<EnumSides, Integer> edges = new HashMap<>();
-		edges.put(EnumSides.LEFT, 1);
-		edges.put(EnumSides.TOP, -1);
-		edges.put(EnumSides.RIGHT, 1);
-		edges.put(EnumSides.BOTTOM, 1);
-		Piece pc1 = new Piece(10, edges);
+        ArrayList<Piece> pcs = new ArrayList<>();
+        pcs.add(pc1);
+        AnalyzeInputs analyze = new AnalyzeInputs(pcs);
+        FileManager fileManager = new FileManager("");
+        analyze.validateEdgesSum();
+        assertTrue(fileManager.getAllErrors().contains("Cannot solve puzzle: edges sum is not zero"));
+    }
 
-		ArrayList<Piece> pcs = new ArrayList<>();
-		pcs.add(pc1);
-		errors = AnalyzeInputs.validateEdgesSum(pcs);
-		assertTrue(errors.contains("Cannot solve puzzle: edges sum is not zero"));
-	}
+    @Test
+    public void ValidatePieceFormatGoodTest() {
+        // Format of edges for single piece is good: 0,0,1,0
+        HashMap<EnumSides, Integer> edges = new HashMap<>();
+        edges.put(EnumSides.LEFT, 0);
+        edges.put(EnumSides.TOP, 0);
+        edges.put(EnumSides.RIGHT, 1);
+        edges.put(EnumSides.BOTTOM, 0);
+        Piece pc1 = new Piece(11, edges);
 
-	@Test
-	void ValidatePieceFormatGoodTest() {
-		// Format of edges for single piece is good: 0,0,1,0
+        ArrayList<Piece> pcs = new ArrayList<>();
+        pcs.add(pc1);
+        AnalyzeInputs analyze = new AnalyzeInputs(pcs);
+        FileManager fileManager = new FileManager("");
+        analyze.validatePiecesFormat();
+        assertFalse(fileManager.getAllErrors().contains("Wrong elements format: 11"));
 
-		HashMap<EnumSides, Integer> edges = new HashMap<>();
-		edges.put(EnumSides.LEFT, 0);
-		edges.put(EnumSides.TOP, 0);
-		edges.put(EnumSides.RIGHT, 1);
-		edges.put(EnumSides.BOTTOM, 0);
-		Piece pc1 = new Piece(11, edges);
+    }
 
-		ArrayList<Piece> pcs = new ArrayList<>();
-		pcs.add(pc1);
-		errors = AnalyzeInputs.validatePiecesFormat(pcs);
-		assertFalse(errors.contains("Wrong elements format: 11"));
+    @Test
+    public void ValidatePieceFormatBadTest() {
+        // Format of edges for single piece is bad: 0,0,2,0
 
-	}
+        HashMap<EnumSides, Integer> edges = new HashMap<>();
+        edges.put(EnumSides.LEFT, 0);
+        edges.put(EnumSides.TOP, 2);
+        edges.put(EnumSides.RIGHT, 0);
+        edges.put(EnumSides.BOTTOM, 0);
+        Piece pc1 = new Piece(13, edges);
 
-	@Test
-	void ValidatePieceFormatBadTest() {
-		// Format of edges for single piece is bad: 0,0,2,0
-		errors.clear();
-		HashMap<EnumSides, Integer> edges = new HashMap<>();
-		edges.put(EnumSides.LEFT, 0);
-		edges.put(EnumSides.TOP, 2);
-		edges.put(EnumSides.RIGHT, 0);
-		edges.put(EnumSides.BOTTOM, 0);
-		Piece pc1 = new Piece(13, edges);
+        ArrayList<Piece> pcs = new ArrayList<>();
+        pcs.add(pc1);
+        FileManager fileManager = new FileManager("");
+        AnalyzeInputs analyze = new AnalyzeInputs(pcs);
+        analyze.validatePiecesFormat();
 
-		ArrayList<Piece> pcs = new ArrayList<>();
-		pcs.add(pc1);
-		errors = AnalyzeInputs.validatePiecesFormat(pcs);
+        assertTrue(fileManager.getAllErrors().contains("Wrong elements value on line: 13"));
 
-		assertTrue(errors.contains("Wrong elements value on line: 13"));
+    }
 
-	}
+    @Test
+    public void ValidateWrongNumberOfStraightEdgesBadTest() {
+        // Less than minimum number of straight edges
 
-	@Test
-	void ValidateWrongNumberOfStraightEdgesBadTest() {
-		// Less than minimum number of straight edges
-		HashMap<EnumSides, Integer> edges = new HashMap<>();
-		edges.put(EnumSides.LEFT, 0);
-		edges.put(EnumSides.TOP, 1);
-		edges.put(EnumSides.RIGHT, 1);
-		edges.put(EnumSides.BOTTOM, 0);
-		Piece pc1 = new Piece(10, edges);
+        HashMap<EnumSides, Integer> edges = new HashMap<>();
+        edges.put(EnumSides.LEFT, 0);
+        edges.put(EnumSides.TOP, 1);
+        edges.put(EnumSides.RIGHT, 1);
+        edges.put(EnumSides.BOTTOM, 0);
+        Piece pc1 = new Piece(10, edges);
 
-		HashMap<EnumSides, Integer> edges2 = new HashMap<>();
-		edges2.put(EnumSides.LEFT, 0);
-		edges2.put(EnumSides.TOP, 1);
-		edges2.put(EnumSides.RIGHT, 0);
-		edges2.put(EnumSides.BOTTOM, 1);
-		Piece pc2 = new Piece(11, edges2);
-		ArrayList<Piece> pcs = new ArrayList<>();
-		pcs.add(pc1);
-		pcs.add(pc2);
+        HashMap<EnumSides, Integer> edges2 = new HashMap<>();
+        edges2.put(EnumSides.LEFT, 0);
+        edges2.put(EnumSides.TOP, 1);
+        edges2.put(EnumSides.RIGHT, 0);
+        edges2.put(EnumSides.BOTTOM, 1);
+        Piece pc2 = new Piece(11, edges2);
+        ArrayList<Piece> pcs = new ArrayList<>();
+        pcs.add(pc1);
+        pcs.add(pc2);
+        AnalyzeInputs analyze = new AnalyzeInputs(pcs);
+        assertTrue(analyze.validateMinimumStraightEdges().isEmpty());
 
-		assertTrue(AnalyzeInputs.validateMinimumStraightEdges(pcs).isEmpty());
+    }
 
-	}
+    @Test
+    public void ValidateNumberOfStraightEdgesGoodTest() {
+        // input has minimum+ number of straight edges
 
-	@Test
-	void ValidateNumberOfStraightEdgesGoodTest() {
-		// input has minimum+ number of straight edges
-		HashMap<EnumSides, Integer> edges = new HashMap<>();
-		edges.put(EnumSides.LEFT, 0);
-		edges.put(EnumSides.TOP, 0);
-		edges.put(EnumSides.RIGHT, 0);
-		edges.put(EnumSides.BOTTOM, 0);
-		Piece pc1 = new Piece(10, edges);
+        HashMap<EnumSides, Integer> edges = new HashMap<>();
+        edges.put(EnumSides.LEFT, 0);
+        edges.put(EnumSides.TOP, 0);
+        edges.put(EnumSides.RIGHT, 0);
+        edges.put(EnumSides.BOTTOM, 0);
+        Piece pc1 = new Piece(10, edges);
 
-		HashMap<EnumSides, Integer> edges2 = new HashMap<>();
-		edges2.put(EnumSides.LEFT, 0);
-		edges2.put(EnumSides.TOP, 0);
-		edges2.put(EnumSides.RIGHT, 0);
-		edges2.put(EnumSides.BOTTOM, 0);
-		Piece pc2 = new Piece(11, edges2);
-		ArrayList<Piece> pcs = new ArrayList<>();
-		pcs.add(pc1);
-		pcs.add(pc2);
+        HashMap<EnumSides, Integer> edges2 = new HashMap<>();
+        edges2.put(EnumSides.LEFT, 0);
+        edges2.put(EnumSides.TOP, 0);
+        edges2.put(EnumSides.RIGHT, 0);
+        edges2.put(EnumSides.BOTTOM, 0);
+        Piece pc2 = new Piece(11, edges2);
+        ArrayList<Piece> pcs = new ArrayList<>();
+        pcs.add(pc1);
+        pcs.add(pc2);
+        AnalyzeInputs analyze = new AnalyzeInputs(pcs);
+        assertFalse(analyze.validateMinimumStraightEdges().isEmpty());
 
-		assertFalse(AnalyzeInputs.validateMinimumStraightEdges(pcs).isEmpty());
+    }
 
-	}
+    @Test
+    public void ValidatePiecesCornersGoodTest() {
+        // Input has minimum+ corners
 
-	@Test
-	void ValidatePiecesCornersGoodTest() {
-		// Input has minimum+ corners
-		errors.clear();
-		HashMap<EnumSides, Integer> edges = new HashMap<>();
-		edges.put(EnumSides.LEFT, 0);
-		edges.put(EnumSides.TOP, 0);
-		edges.put(EnumSides.RIGHT, 0);
-		edges.put(EnumSides.BOTTOM, 0);
-		Piece pc1 = new Piece(10, edges);
+        HashMap<EnumSides, Integer> edges = new HashMap<>();
+        edges.put(EnumSides.LEFT, 0);
+        edges.put(EnumSides.TOP, 0);
+        edges.put(EnumSides.RIGHT, 0);
+        edges.put(EnumSides.BOTTOM, 0);
+        Piece pc1 = new Piece(10, edges);
 
-		HashMap<EnumSides, Integer> edges2 = new HashMap<>();
-		edges2.put(EnumSides.LEFT, 0);
-		edges2.put(EnumSides.TOP, 0);
-		edges2.put(EnumSides.RIGHT, 0);
-		edges2.put(EnumSides.BOTTOM, 0);
-		Piece pc2 = new Piece(11, edges2);
-		ArrayList<Piece> pcs = new ArrayList<>();
-		pcs.add(pc1);
-		pcs.add(pc2);
+        HashMap<EnumSides, Integer> edges2 = new HashMap<>();
+        edges2.put(EnumSides.LEFT, 0);
+        edges2.put(EnumSides.TOP, 0);
+        edges2.put(EnumSides.RIGHT, 0);
+        edges2.put(EnumSides.BOTTOM, 0);
+        Piece pc2 = new Piece(11, edges2);
+        ArrayList<Piece> pcs = new ArrayList<>();
+        pcs.add(pc1);
+        pcs.add(pc2);
+        AnalyzeInputs analyze = new AnalyzeInputs(pcs);
+        FileManager fileManager = new FileManager("");
+        analyze.validateMinimumCorners();
+        assertFalse(fileManager.getAllErrors().contains("Cannot solve puzzle: missing corner element: "));
 
-		errors = AnalyzeInputs.validateMinimumCorners(pcs);
-		assertFalse(errors.contains("Cannot solve puzzle: missing corner element: "));
+    }
 
-	}
+    @Test
+    public void ValidatePiecesCornersBadTest() {
+        // Less than minimum corners
 
-	@Test
-	void ValidatePiecesCornersBadTest() {
-		// Less than minimum corners
-		errors.clear();
+        HashMap<EnumSides, Integer> edges = new HashMap<>();
+        edges.put(EnumSides.LEFT, 0);
+        edges.put(EnumSides.TOP, 1);
+        edges.put(EnumSides.RIGHT, 1);
+        edges.put(EnumSides.BOTTOM, 0);
+        Piece pc1 = new Piece(10, edges);
 
-		HashMap<EnumSides, Integer> edges = new HashMap<>();
-		edges.put(EnumSides.LEFT, 0);
-		edges.put(EnumSides.TOP, 1);
-		edges.put(EnumSides.RIGHT, 1);
-		edges.put(EnumSides.BOTTOM, 0);
-		Piece pc1 = new Piece(10, edges);
+        HashMap<EnumSides, Integer> edges2 = new HashMap<>();
+        edges2.put(EnumSides.LEFT, 0);
+        edges2.put(EnumSides.TOP, -1);
+        edges2.put(EnumSides.RIGHT, -1);
+        edges2.put(EnumSides.BOTTOM, 0);
+        Piece pc2 = new Piece(11, edges2);
+        ArrayList<Piece> pcs = new ArrayList<>();
+        pcs.add(pc1);
+        pcs.add(pc2);
+        AnalyzeInputs analyze = new AnalyzeInputs(pcs);
+        FileManager fileManager = new FileManager("");
+        analyze.validateMinimumCorners();
+        assertTrue(fileManager.getAllErrors().contains("Cannot solve puzzle: missing corner element: BR"));
+        assertTrue(fileManager.getAllErrors().contains("Cannot solve puzzle: missing corner element: TR"));
+        assertTrue(fileManager.getAllErrors().contains("Cannot solve puzzle: missing corner element: TL"));
 
-		HashMap<EnumSides, Integer> edges2 = new HashMap<>();
-		edges2.put(EnumSides.LEFT, 0);
-		edges2.put(EnumSides.TOP, -1);
-		edges2.put(EnumSides.RIGHT, -1);
-		edges2.put(EnumSides.BOTTOM, 0);
-		Piece pc2 = new Piece(11, edges2);
-		ArrayList<Piece> pcs = new ArrayList<>();
-		pcs.add(pc1);
-		pcs.add(pc2);
+    }
 
-		errors = AnalyzeInputs.validateMinimumCorners(pcs);
-		assertTrue(errors.contains("Cannot solve puzzle: missing corner element: BR"));
-		assertTrue(errors.contains("Cannot solve puzzle: missing corner element: TR"));
-		assertTrue(errors.contains("Cannot solve puzzle: missing corner element: TL"));
+    @Test
+    public void AnalyzeTest() throws IOException {
+        String piecesFile = "./src/test/java/AnalyzeinputTests/resourcesFiles/test1.in";
+        FileManager fileManagment = new FileManager(piecesFile);
+        ArrayList<Piece> pieces = fileManagment.setPiecesFromFile();
+        AnalyzeInputs analyze = new AnalyzeInputs(pieces);
+        analyze.analyzePicesList();
 
-	}
-
-	@Test
-	public void AnalyzeTest() throws IOException {
-		String piecesFile = "C:\\BiqPassoverProject\\test1.in";
-		String outputFile = "C:\\BiqPassoverProject\\test1.out";
-		FileManagment fileManagment = new FileManagment(piecesFile);
-		ArrayList<Piece> pieces = fileManagment.getPicesFromFile();
-		AnalyzeInputs.analyzePicesList(pieces, outputFile);
-
-	}
+    }
 }
