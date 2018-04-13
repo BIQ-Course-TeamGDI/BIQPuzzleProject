770c59ea32876a58dd0277fc663a8d5135f155e9
gb824v <gb824v@intl.att.com>
2018-04-13 12:06:18 +0300

    AnalyzeInputs updates

--- main/src/main/java/puzzle/Main.java
+++ main/src/main/java/puzzle/Main.java
@@ -1,14 +1,16 @@
 package puzzle;
+
 import puzzle.PuzzleManager;
 
 import java.io.IOException;
 
-public class Main {
+public class Main
+{
 
-
-    public static void main(String[] args) throws IOException {
-        PuzzleManager puzzleManager = new PuzzleManager(args[0],args[1]);
-        puzzleManager.solvePuzzle();
-    }
+	public static void main(String[] args) throws IOException
+	{
+		PuzzleManager puzzleManager = new PuzzleManager(args[0], args[1]);
+		puzzleManager.solvePuzzle();
+	}
 
 }
