f00134c47569b0fcdb551374213ecfa5301ef5c0
gb824v <gb824v@intl.att.com>
2018-04-13 11:36:19 +0300

    AnalyzeInputs updates

--- main/src/main/java/puzzle/Main.java
+++ main/src/main/java/puzzle/Main.java
@@ -7,8 +7,6 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-        args[0] = "";
-        args[1] = "";
         PuzzleManager puzzleManager = new PuzzleManager(args[0],args[1]);
         puzzleManager.solvePuzzle();
     }
