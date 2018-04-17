cf57322aba21cd0cdaa979c86e4c7cbb03842f7e
Doron Niv <dn141b@intl.att.com>
2018-04-17 23:02:08 +0300

    Fixed due to Amir comments

--- main/src/main/java/puzzle/infra/FileManager.java
+++ main/src/main/java/puzzle/infra/FileManager.java
@@ -1,4 +1,4 @@
-package infra;
+package puzzle.infra;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -6,8 +6,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import puzzle.Piece;
 
